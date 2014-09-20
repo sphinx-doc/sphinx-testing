39510355bb51979f07a983f58ea303c2d3604471
Takeshi KOMIYA <tk0miya@users.noreply.github.com>
2014-09-20 18:27:27 +0900

    Remove unused codes from sphinx/util.py

--- src/sphinx/testing/util.py
+++ src/sphinx/testing/util.py
@@ -7,103 +7,16 @@
     :license: BSD, see LICENSE for details.
 """
 
-import os
-import sys
-import tempfile
 import shutil
-import re
-from functools import wraps
-
+import tempfile
 from six import StringIO
+from functools import wraps
 
 from sphinx import application
 from sphinx.theming import Theme
 from sphinx.ext.autodoc import AutoDirective
 
 from path import path
-
-from nose import tools, SkipTest
-
-try:
-    # Python >=3.3
-    from unittest import mock
-except ImportError:
-    import mock
-
-
-__all__ = [
-    'test_root', 'test_roots', 'raises', 'raises_msg',
-    'skip_if', 'skip_unless', 'skip_unless_importable', 'Struct',
-    'ListOutput', 'TestApp', 'with_app', 'gen_with_app',
-    'path', 'with_tempdir',
-    'sprint', 'remove_unicode_literals',
-    'mock',
-]
-
-
-test_root = path(__file__).parent.joinpath('root').abspath()
-test_roots = path(__file__).parent.joinpath('roots').abspath()
-
-
-def _excstr(exc):
-    if type(exc) is tuple:
-        return str(tuple(map(_excstr, exc)))
-    return exc.__name__
-
-def raises(exc, func, *args, **kwds):
-    """
-    Raise :exc:`AssertionError` if ``func(*args, **kwds)`` does not
-    raise *exc*.
-    """
-    try:
-        func(*args, **kwds)
-    except exc:
-        pass
-    else:
-        raise AssertionError('%s did not raise %s' %
-                             (func.__name__, _excstr(exc)))
-
-def raises_msg(exc, msg, func, *args, **kwds):
-    """
-    Raise :exc:`AssertionError` if ``func(*args, **kwds)`` does not
-    raise *exc*, and check if the message contains *msg*.
-    """
-    try:
-        func(*args, **kwds)
-    except exc as err:
-        assert msg in str(err), "\"%s\" not in \"%s\"" % (msg, err)
-    else:
-        raise AssertionError('%s did not raise %s' %
-                             (func.__name__, _excstr(exc)))
-
-def skip_if(condition, msg=None):
-    """Decorator to skip test if condition is true."""
-    def deco(test):
-        @tools.make_decorator(test)
-        def skipper(*args, **kwds):
-            if condition:
-                raise SkipTest(msg or 'conditional skip')
-            return test(*args, **kwds)
-        return skipper
-    return deco
-
-def skip_unless(condition, msg=None):
-    """Decorator to skip test if condition is false."""
-    return skip_if(not condition, msg)
-
-def skip_unless_importable(module, msg=None):
-    """Decorator to skip test if module is not importable."""
-    try:
-        __import__(module)
-    except ImportError:
-        return skip_if(True, msg)
-    else:
-        return skip_if(False, msg)
-
-
-class Struct(object):
-    def __init__(self, **kwds):
-        self.__dict__.update(kwds)
 
 
 class ListOutput(object):
@@ -137,11 +50,9 @@ class TestApp(application.Sphinx):
 
         application.CONFIG_FILENAME = confname
 
-        self.cleanup_trees = [test_root / 'generated']
+        self.cleanup_trees = []
 
-        if srcdir is None:
-            srcdir = test_root
-        elif srcdir == '(empty)':
+        if srcdir == '(empty)':
             tempdir = path(tempfile.mkdtemp())
             self.cleanup_trees.append(tempdir)
             temproot = tempdir / 'root'
@@ -213,23 +124,6 @@ def with_app(*args, **kwargs):
     return generator
 
 
-def gen_with_app(*args, **kwargs):
-    """
-    Decorate a test generator to pass a TestApp as the first argument to the
-    test generator when it's executed.
-    """
-    def generator(func):
-        @wraps(func)
-        def deco(*args2, **kwargs2):
-            app = TestApp(*args, **kwargs)
-            for item in func(app, *args2, **kwargs2):
-                yield item
-            # don't execute cleanup if test failed
-            app.cleanup()
-        return deco
-    return generator
-
-
 def with_tempdir(func):
     def new_func(*args, **kwds):
         tempdir = path(tempfile.mkdtemp())
@@ -237,19 +131,3 @@ def with_tempdir(func):
         tempdir.rmtree()
     new_func.__name__ = func.__name__
     return new_func
-
-
-def sprint(*args):
-    sys.stderr.write(' '.join(map(str, args)) + '\n')
-
-_unicode_literals_re = re.compile(r'u(".*?")|u(\'.*?\')')
-def remove_unicode_literals(s):
-    return _unicode_literals_re.sub(lambda x: x.group(1) or x.group(2), s)
-
-
-def find_files(root, suffix=None):
-    for dirpath, dirs, files in os.walk(root, followlinks=True):
-        dirpath = path(dirpath)
-        for f in [f for f in files if not suffix or f.endswith(suffix)]:
-            fpath = dirpath / f
-            yield os.path.relpath(fpath, root)
