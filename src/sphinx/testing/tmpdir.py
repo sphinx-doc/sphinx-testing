5b45f4911af29b71d6e6d560ea979f05ca3eaa8b
Takeshi KOMIYA <tk0miya@users.noreply.github.com>
2014-09-20 18:25:34 +0900

    Fix with_tempdir() does not clean files and directory if exception raised

--- src/sphinx/testing/tmpdir.py
+++ src/sphinx/testing/tmpdir.py
@@ -8,13 +8,17 @@
 """
 
 import tempfile
+from functools import wraps
 from sphinx.testing.path import path
 
 
 def with_tempdir(func):
-    def new_func(*args, **kwds):
-        tempdir = path(tempfile.mkdtemp())
-        func(tempdir, *args, **kwds)
-        tempdir.rmtree()
-    new_func.__name__ = func.__name__
-    return new_func
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        try:
+            tmpdir = path(tempfile.mkdtemp())
+            args = args + (tmpdir,)  # extends argument; add tmpdir at tail
+            return func(*args, **kwargs)
+        finally:
+            tmpdir.rmtree()
+    return decorator
