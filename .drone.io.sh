327eb9f27078bac4c91deef355e1c080870afc04
Takeshi KOMIYA <tk0miya@users.noreply.github.com>
2014-09-24 14:13:29 +0900

    Add .drone.io.sh

---
+++ .drone.io.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get update
+sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev
+
+pip install --use-mirrors --upgrade detox misspellings check-manifest docutils
+find src/ -name "*.py" | misspellings -f -
+detox
+check-manifest
