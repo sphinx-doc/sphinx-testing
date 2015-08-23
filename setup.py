95b943881655c456ec05dd6bf66ebd26a9a93b07
Takeshi KOMIYA <i.tkomiya@gmail.com>
2015-08-23 23:27:40 +0900

    Update classifiers

--- setup.py
+++ setup.py
@@ -4,18 +4,26 @@ from setuptools import setup, find_packages
 
 classifiers = [
     "Development Status :: 4 - Beta",
+    "Environment :: Console",
+    "Environment :: Web Environment",
+    "Framework :: Sphinx :: Extension",
+    "Intended Audience :: Developers",
     "Intended Audience :: System Administrators",
     "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3.2",
     "Programming Language :: Python :: 3.3",
     "Programming Language :: Python :: 3.4",
+    "Topic :: Documentation",
+    "Topic :: Documentation :: Sphinx",
     "Topic :: Software Development",
     "Topic :: Software Development :: Documentation",
     "Topic :: Software Development :: Testing",
     "Topic :: Text Processing :: Markup",
+    "Topic :: Utilities",
 ]
 
 test_require = []
